3998d58eab6759a4c592e3900c79863cf01cc2ed
liuhuan <qq384012351@gmail.com>
2017-03-02 00:06:57 +0800

    crawl the infomation of the pandatv first page's anchors

--- pandatv/spiders/anchor.py
+++ pandatv/spiders/anchor.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from pandatv.allAnchorsItems import AnchorsItems
 
 class AnchorSpider(scrapy.Spider):
     name = "anchor"
@@ -8,8 +8,13 @@ class AnchorSpider(scrapy.Spider):
     start_urls = ['http://www.panda.tv/all']
 
     def parse(self, response):
-        sel = scrapy.Selector(response)
-        lis = response.selector.xpath('//ul[@id="later-play-list"]/li').extract()
+        prefix = 'http://www.panda.tv'
+        item = AnchorsItems()
+        lis = response.selector.xpath('//ul[@id="later-play-list"]/li')
         for content in lis: 
-            print(content)
-            break;
+            item['anchorUrl'] = prefix+ content.xpath('.//a/@href').extract()[0]
+            item['videoTitle'] = content.xpath('.//div[@class="video-title"]/@title').extract()[0]
+            item['videoNickName'] = content.xpath('.//div[@class="video-info"]/span[@class="video-nickname"]/text()').extract()[0]
+            item['videoNumber'] = content.xpath('.//div[@class="video-info"]/span[@class="video-number"]/text()').extract()[0]
+            item['videoCategory'] = content.xpath('.//div[@class="video-info"]/span[@class="video-cate"]/text()').extract()[0]
+            yield item
